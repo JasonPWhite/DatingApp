683dfe164f42c8f4f461c869e1241c55cc22a990
Jason White <jason.white@spicerhaart.co.uk>
2019-02-13 08:47:51 +0000

    end of section 4

---
+++ DatingApp-SPA/src/app/heatMap/heatMap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heatMap',
+  templateUrl: './heatMap.component.html',
+  styleUrls: ['./heatMap.component.css']
+})
+export class HeatMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
